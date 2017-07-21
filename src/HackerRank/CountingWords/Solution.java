d7827b37fb671817292d02e4c152ffc08a0db8ea
nsimsiri <nsimsiri@umass.edu>
2017-07-21 19:30:59 +0700

    Saving

--- src/HackerRank/CountingWords/Solution.java
+++ src/HackerRank/CountingWords/Solution.java
@@ -9,6 +9,7 @@ import java.util.regex.*;
 public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("hi");
 
     }
 }
