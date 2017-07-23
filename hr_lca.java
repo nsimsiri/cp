e8ecc1690a45a0192f15a8aabad598e18da4d970
nsimsiri <nsimsiri@umass.edu>
2017-07-23 21:47:59 +0700

    Saving

--- hr_lca.java
+++ hr_lca.java
@@ -13,7 +13,16 @@ public class hr_lca {
     }
     
     static Node _lca(Node root, int a, int b){
-        return null;
+        Map<Integer,Integer> m = new HashMap<>();
+        int[] In=new int[]{a,b};
+        for(int i=0;i<2;i++){
+            Stack<Node> st = new Stack<>();
+            st.push(In[i]);
+            boolean found = false
+            while(!found && !st.isEmpty()){
+                
+            }
+        }    
     }
     public static void main(String[] args){
         System.out.println("hilekj");
