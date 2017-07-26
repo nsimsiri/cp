ac68afe50ccb2c899c320fad21263d8b90be7b45
nsimsiri <nsimsiri@umass.edu>
2017-07-26 16:04:01 +0700

    Saving

--- hr_sn.java
+++ hr_sn.java
@@ -1,8 +1,34 @@
 import java.util.;
 
 public class hr_sn {
+    public static class Node{
+        Node left; Node right; int data;
+        public Node(Node left, int data, Node right){
+            this.left=left; this.right=right; this.data=data;
+        }
+        @Override public String toString(){
+            return "Node("+this.left+", "+this.data+", "+thie.right+")";
+        }
+    }
     public static void(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+        int N = Integer.parseInt(in.readLine());
+        Node[] vtx = new Node[];
+        Node n1 = new(null, 1, null);
+        vtx[0] = n1;
+        for(int i = 0; i < N; i++){
+            String ss = in.readLine();
+            String[] sl = ss.split(" ");
+            Node a = new Node(null, Integer.parseInt(sl[0]), null);
+            Node b = new Node(null, Integer.parseInt(sl[1]), null);
+            vtx[i+1].left = a; vtx[i+1].right = b;
+        }
+        int T = Integer.parseInt(in.readLine());
+        for (int i = 0; i < T; i++){
+            int lv = Integer.parseInt(in.readLine());
+            System.out.println(lv);
+        }
         
     }
 }
+
