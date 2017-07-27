ed6ffec818286a4684007ef61fddcce2c7a93bac
nsimsiri <nsimsiri@umass.edu>
2017-07-27 17:02:43 +0700

    Saving

--- hr_isbst.java
+++ hr_isbst.java
@@ -5,4 +5,22 @@ public class hr_isbst{
         boolean b = (root.right == null) true : root.right.data > root.data;
         return a && && b && checkBst(root.left) && checkBst(root.right);
     }
+    void inotr(Node root, List<Integer> In){
+        if(root.left != null){
+            inotr(root.left, In);
+        }
+        In.add(In.data);
+        if(root.right != null){
+            inotr(root.right, In);
+        }
+    }
+    boolean checkBST(Node root){
+        List<Integer> In = new ArrayList<>();
+        inotr(root, In);
+        for(int i = 0; i < In.size()-1; i++){
+            if (In.get(i) >= In.get(i+1)) return false;
+        }
+        return true;
+        
+    }
 }
