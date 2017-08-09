de633348a321a8ba9ef104ada890c1d9a9d8cf10
nsimsiri <nsimsiri@umass.edu>
2017-08-09 13:45:12 +0700

    Saving

--- hr_mabsd.java
+++ hr_mabsd.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class hr_mabsd {
     public static void main(String[] args){
-        
+        Arrays.sort(a);
+        int min = Math.abs(a[0]-a[1]);
+        for(int i = 0;i<n-1;i++){
+            int t = Math.abs(a[i+1], a[i]);
+            min = (min > t) ? t : min;
+        }
+        System.out.println(min);        
     }
 }
