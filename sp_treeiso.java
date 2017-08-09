48cb0f4f817909fa12356a681b41c7582a51a611
nsimsiri <nsimsiri@umass.edu>
2017-08-09 20:20:38 +0700

    Saving

--- sp_treeiso.java
+++ sp_treeiso.java
@@ -1,24 +1,38 @@
 import java.util.*;
 
 public class sp_treeiso {
+    public static void bldtr(int a,int b,Map<Integer,List<Integer>>){
+        if(m.containsKey(a)) m.get(a).add(b);
+        else{
+            List<Integer> l = new ArraList<>();
+            l.add(b);
+            m.put(a,l);
+        }
+        if(m.containsKey(b)) m.get(b).add(a);
+        else {
+            List<Integer> l = new ArrayList<>();
+            l.add(a);
+            m.put(b,l);
+        }
+    }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
             int k = nextInt();
-            int[][] t1 = new int[n-1][2];
-            int[][] t2 = new int[n-1][2];
+            Map<Integer,List<Integer>> t1 = new Map<>();
+            Map<Integer,List<Integer>> t2 = new Map<>();
             for(int j = 0;j<2*n-2;j++){
                 String[] inp = in.nextLine().split(" ");
                 int v1 = Integer.parseInt(inp[0]);
                 int v2 = Integer.parseInt(inp[1]);
-                if(j<n){
-                    t1[j]= new int[]{v1,v2};
-                } else {
-                    t2[j-n]= new int[]{v1,v2};
-                }
+                if(j<n) bldtr(v1,v2,t1);
+                else bldtr(v1,v2,t2);
+                    
+              
             }
            
         }
     }
 }
+
