fcb34c005951862156d0e7e148d2eb402345c524
nsimsiri <nsimsiri@umass.edu>
2017-08-09 19:39:24 +0700

    Saving

--- sp_treeiso.java
+++ sp_treeiso.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class sp_treeiso {
     public static void main(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            
+        }
     }
 }
