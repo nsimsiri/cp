12250f250a4c1b2c9a10011e5e804cfc81b0136e
nsimsiri <nsimsiri@umass.edu>
2017-08-09 19:51:45 +0700

    Saving

--- sp_treeiso.java
+++ sp_treeiso.java
@@ -5,7 +5,20 @@ public class sp_treeiso {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
-            
+            int k = nextInt();
+            int[][] t1 = new int[n-1][2];
+            int[][] t2 = new int[n-1][2];
+            for(int j = 0;j<2*n-2;j++){
+                String[] inp = in.nextLine().split(" ");
+                int v1 = Integer.parseInt(inp[0]);
+                int v2 = Integer.parseInt(inp[1]);
+                if(j<n){
+                    t1[j]= new int[]{v1,v2};
+                } else {
+                    t2[j-n]= new int[]{v1,v2};
+                }
+            }
+           
         }
     }
 }
