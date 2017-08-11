3c1d924de477896abdfe8aa6b2e18579c328cebb
nsimsiri <nsimsiri@umass.edu>
2017-08-11 11:01:19 +0700

    Saving

--- hr_cupcal.java
+++ hr_cupcal.java
@@ -4,7 +4,7 @@ public class hr_cupcal{
         Arrays.sort(calories);
         int s =0;
         for(int i = n-1; i >= 0;i--){
-            s+= Math.pow(2, i-n+1)*calories[i];
+            s+= Math.pow(2, i-n-1)*calories[i];
         }
         System.out.println(s);
     }
