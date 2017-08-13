68e4b42af30126d88cf2a7f61ac58037906781ce
nsimsiri <nsimsiri@umass.edu>
2017-08-13 14:23:45 +0700

    Saving

--- hr_catmouse.java
+++ hr_catmouse.java
@@ -1,6 +1,14 @@
 import java.util.*;
 public class hr_catmouse {
     public static void main(String[] args){
-        
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        for(int i = 0; i < N; i++){
+            String[] line = in.readLine().split(" ");
+            int a = Integer.parseInt(line[0]);
+            int b = Integer.parseInt(line[1]);
+            int c = Integer.parseInt(line[2]);
+            
+        }
     }
 }
