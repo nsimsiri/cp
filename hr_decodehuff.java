a075d852b61e0e734eac8d91fafecd276a309676
nsimsiri <nsimsiri@umass.edu>
2017-08-15 09:37:45 +0700

    Saving

--- hr_decodehuff.java
+++ hr_decodehuff.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class hr_decodehuff{
     void decode(String S, Node root){
-        
+        int i = 0
+        while(S.length() > i){
+            char c = S.charAt(i);
+            
+        }
     }
 }
